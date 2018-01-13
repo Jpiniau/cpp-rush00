9ed735992b3948e85e2d8a167d4aeae6e4ea3336
Jean-baptiste PINIAU <jpiniau@e1r7p14.42.fr>
2018-01-13 16:07:47 +0100

    IPlayer ISpaceShip ;

--- IPlayer.hpp
+++ IPlayer.hpp
@@ -6,7 +6,7 @@
 /*   By: jpiniau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/13 13:08:06 by jpiniau           #+#    #+#             */
-/*   Updated: 2018/01/13 15:47:08 by jpiniau          ###   ########.fr       */
+/*   Updated: 2018/01/13 16:07:15 by jpiniau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ class IPlayer
 		virtual int*	detectInput(void) const = 0;
 		virtual void	setCoord(void) const = 0;
 
-}
+};
 
 #endif
--- ISpaceShip.hpp
+++ ISpaceShip.hpp
@@ -6,7 +6,7 @@
 /*   By: jpiniau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/13 13:03:54 by jpiniau           #+#    #+#             */
-/*   Updated: 2018/01/13 15:53:25 by jpiniau          ###   ########.fr       */
+/*   Updated: 2018/01/13 16:06:21 by jpiniau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ class ISpaceShip
 		virtual ~ISpaceShip(void) const = 0;
 
 		virtual void	shoot(void) const = 0;
-}
+};
 
 #endif
